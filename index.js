5e417fd9b71027a55291f9d7a51c8a26794dc77d
vincenteliang <vincenteljh@gmail.com>
2020-07-12 17:30:00 +0800

    4-3 使用 koa-router 实现路由
    高级路由功能

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const koa = require('koa');
 const Router = require('koa-router')
 const app = new koa();
 const router = new Router();
+const usersRouter = new Router({ prefix: '/users' })
 
-router.get('/', (ctx) => {
+const auth = async (ctx, next) => {
+    // 错误前置
+    if (ctx.url !== 'users') {
+        ctx.throw(401);
+    }
+    await next();
+}
+
+router.get('/', auth, (ctx) => {
     ctx.body = '这是主页';
 })
 
-router.get('/users', (ctx) => {
+usersRouter.get('/', auth, (ctx) => {
     ctx.body = '这是用户列表';
 })
 
-router.post('/users', (ctx) => {
+usersRouter.post('/', auth, (ctx) => {
     ctx.body = '创建用户';
 })
 
-router.get('/users/:id', (ctx) => {
+usersRouter.get('/:id', auth, (ctx) => {
     ctx.body = `这是用户 ${ctx.params.id}`;
 })
 
 app.use(router.routes());
+app.use(usersRouter.routes());
 
 app.listen(3000);
