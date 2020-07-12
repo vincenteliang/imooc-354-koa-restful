7a28c4d7dfcc24334934c0eff59816a28f0c8bea
vincenteliang <vincenteljh@gmail.com>
2020-07-12 16:50:43 +0800

    3-3 Koa 中间件与洋葱模型

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const koa = require('koa');
 const app = new koa();
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(2);
     ctx.body = 'Hello World';
+});
+app.use(async (ctx, next) => {
+    console.log(3);
+    await next();
+    console.log(4);
+});
+app.use(async (ctx) => {
+    console.log(5);
 });
 
 app.listen(3000);
+// 打印顺序为：13542
