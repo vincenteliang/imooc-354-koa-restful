318d579ce4639b442a12ff18d7a24d03f4e0087f
vincenteliang <vincenteljh@gmail.com>
2020-07-12 17:23:18 +0800

    4-3 使用 koa-router 实现路由
    路由基本功能

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const koa = require('koa');
+const Router = require('koa-router')
 const app = new koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-    if (ctx.url === '/') {
-        ctx.body = '这是主页'
-    } else if (ctx.url === '/users') {
-        if (ctx.method === 'GET') {
-            ctx.body = '获取用户';
-        } else if (ctx.method === 'POST') {
-            ctx.body = '创建用户';
-        } else {
-            ctx.status = 405;
-        }
-    } else if (ctx.url.match(/\/users\/\w+/)) {
-        // match方法返回一个数组，0号元素是url本身，1号为小括号里的内容
-        const userId = ctx.url.match(/\/users\/(\w+)/)[1];
-        ctx.body = `这是用户 ${userId}`
-    } else {
-        ctx.status = 404;
-    }
-});
+router.get('/', (ctx) => {
+    ctx.body = '这是主页';
+})
+
+router.get('/users', (ctx) => {
+    ctx.body = '这是用户列表';
+})
+
+router.post('/users', (ctx) => {
+    ctx.body = '创建用户';
+})
+
+router.get('/users/:id', (ctx) => {
+    ctx.body = `这是用户 ${ctx.params.id}`;
+})
+
+app.use(router.routes());
 
 app.listen(3000);
