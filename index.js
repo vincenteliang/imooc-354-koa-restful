fd4c2302270c0094bd30778b4eff611e4b1ca48b
vincenteliang <vincenteljh@gmail.com>
2020-07-15 11:07:42 +0800

    5-3 发送 HTTP 响应
    添加了一点点注释

--- index.js
+++ index.js
@@ -5,27 +5,40 @@ const app = new koa();
 const router = new Router();
 const usersRouter = new Router({ prefix: '/users' });
 
+// 内存数据库
+const db = [{ name: 'user1' }, { name: 'user2' }];
+
+// 获取根目录
 router.get('/', (ctx) => {
     ctx.body = '这是主页';
 });
 
+// 获取用户列表
 usersRouter.get('/', (ctx) => {
-    ctx.body = [{ name: 'user1' }, { name: 'user2' }];
+    ctx.set('Allow', 'GET', 'POST'); // 设置header
+    ctx.body = db;
 });
 
+// 新建用户
 usersRouter.post('/', (ctx) => {
-    ctx.body = { name: 'user' };
+    db.push(ctx.request.body);
+    ctx.body = ctx.request.body;
 });
 
+// 获取特定id用户
 usersRouter.get('/:id', (ctx) => {
-    ctx.body = { name: ctx.params.id };
+    ctx.body = db[ctx.params.id * 1]; // ID*1转换为数字
 });
 
+// 修改特定id用户
 usersRouter.put('/:id', (ctx) => {
-    ctx.body = { name: ctx.params.id };
+    db[ctx.params.id * 1] = ctx.request.body;
+    ctx.body = ctx.request.body;
 });
 
+// 删除特定id用户
 usersRouter.delete('/:id', (ctx) => {
+    db.splice(ctx.params.id * 1, 1);
     ctx.status = 204;
 });
 
