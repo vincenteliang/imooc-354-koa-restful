b83cff0e2fbbb85334b6cacb45387aa86d31fc84
vincenteliang <vincenteljh@gmail.com>
2020-07-20 17:44:34 +0800

    7-6 用 MongoDB 实现用户的增删改查

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,40 +1,35 @@
-const db = [{ name: 'user1' }, { name: 'user2' }];
+const User = require('../models/users');
 
 class UsersCtl {
-    find(ctx) {
-        a.b  // 故意写一段错误代码
-        ctx.body = db;
+    async find(ctx) {
+        ctx.body = await User.find();
     }
-    findById(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412);
-        }
-        ctx.body = db[ctx.params.id * 1];
+    async findById(ctx) {
+        // findById 方法实现查找
+        const user = await User.findById(ctx.params.id);
+        if (!user) { ctx.throw(404, '用户不存在'); }
+        ctx.body = user;
     }
-    create(ctx) {
+    async create(ctx) {
         ctx.verifyParams({
-            name: { type: 'string' }, // required默认为true
-            age: { type: 'number', required: false }
+            name: { type: 'string', required: true },
         });
-        db.push(ctx.request.body);
-        ctx.body = ctx.request.body;
+        const user = await new User(ctx.request.body).save();
+        ctx.body = user;
     }
-    update(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412);
-        }
+    async update(ctx) {
         ctx.verifyParams({
-            name: { type: 'string' }, // required默认为true
-            age: { type: 'number', required: false }
+            name: { type: 'String', required: true },
         });
-        db[ctx.params.id * 1] = ctx.request.body;
-        ctx.body = ctx.request.body;
+        // findByIdAndUpdate 方法实现更新
+        const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+        if (!user) { ctx.throw(404, '用户不存在'); }
+        ctx.body = user;
     }
-    delete(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412);
-        }
-        db.splice(ctx.params.id * 1, 1);
+    async delete(ctx) {
+        // findByIdAndRemove 方法实现删除
+        const user = await User.findByIdAndRemove(ctx.params.id);
+        if (!user) { ctx.throw(404, '用户不存在'); }
         ctx.status = 204;
     }
 }
