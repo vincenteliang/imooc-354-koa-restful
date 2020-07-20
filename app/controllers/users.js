777be207afd1e821569682b65c84d47b421ced79
vincenteliang <vincenteljh@gmail.com>
2020-07-20 14:49:57 +0800

    6-2 Koa 自带的错误处理

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,9 +2,13 @@ const db = [{ name: 'user1' }, { name: 'user2' }];
 
 class UsersCtl {
     find(ctx) {
+        a.b  // 故意写一段错误代码
         ctx.body = db;
     }
     findById(ctx) {
+        if(ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败: ID大于数组长度');
+        }
         ctx.body = db[ctx.params.id * 1];
     }
     create(ctx) {
