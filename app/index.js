bb723d89a8ea3a5e7129e0bda27f7c78b35d4eca
vincenteliang <vincenteljh@gmail.com>
2020-07-20 15:05:34 +0800

    6-4 使用 koa-json-error 进行错误处理

--- app/index.js
+++ app/index.js
@@ -1,20 +1,12 @@
 const koa = require('koa');
 const bodyparser = require('koa-bodyparser');
+const error = require('koa-json-error');
 const app = new koa();
 const routing = require('./routes');
 
-// 这种方法并不能捕获404报错
-app.use(async (ctx, next) => {
-    try {
-        await next();
-    } catch(err) {
-        ctx.status = err.status || err.statusCode || 500;
-        ctx.body = {
-            message: err.message
-        }
-    }
-});
-
+app.use(error({
+    postFormat: (e,  { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
+}));
 app.use(bodyparser());
 routing(app);
 
