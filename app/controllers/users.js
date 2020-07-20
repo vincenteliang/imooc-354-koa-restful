e6ea2ac4cd3ac96dd1be9673856cc9db9beffa60
vincenteliang <vincenteljh@gmail.com>
2020-07-20 15:14:49 +0800

    6-5 使用 koa-parameter 校验参数

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,20 +6,34 @@ class UsersCtl {
         ctx.body = db;
     }
     findById(ctx) {
-        if(ctx.params.id * 1 >= db.length) {
+        if (ctx.params.id * 1 >= db.length) {
             ctx.throw(412);
         }
         ctx.body = db[ctx.params.id * 1];
     }
     create(ctx) {
+        ctx.verifyParams({
+            name: { type: 'string' }, // required默认为true
+            age: { type: 'number', required: false }
+        });
         db.push(ctx.request.body);
         ctx.body = ctx.request.body;
     }
     update(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412);
+        }
+        ctx.verifyParams({
+            name: { type: 'string' }, // required默认为true
+            age: { type: 'number', required: false }
+        });
         db[ctx.params.id * 1] = ctx.request.body;
         ctx.body = ctx.request.body;
     }
     delete(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412);
+        }
         db.splice(ctx.params.id * 1, 1);
         ctx.status = 204;
     }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const error = require('koa-json-error');
+const parameter = require('koa-parameter');
 const app = new koa();
 const routing = require('./routes');
 
@@ -8,6 +9,7 @@ app.use(error({
     postFormat: (e,  { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
 }));
 app.use(bodyparser());
+app.use(parameter(app)); // 传入app，可以在ctx中加入方法，全局使用
 routing(app);
 
 app.listen(3000, () => console.log('程序启动在3000端口'));
