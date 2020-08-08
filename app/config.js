25221b3fe3298640c31b6597e7951812efadf32e
vincenteliang <vincenteljh@gmail.com>
2020-08-08 17:22:24 +0800

    8-7 自己编写 Koa 中间件实现用户认证与授权

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'zhihu-jwt-secert',
-    connectionStr: 'mongodb+srv://vincente:vincente@vincentebase.yqjup.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    connectionStr: 'mongodb+srv://vincente:vincente@vincentebase.yqjup.mongodb.net/VincenteBase?retryWrites=true&w=majority'
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,12 +1,30 @@
+const jsonwebtoken = require('jsonwebtoken');
 const Router = require('koa-router');
 const router = new Router({ prefix: '/users' });
-const { find, findById, create, update, delete: del, login } = require('../controllers/users');
+const {
+    find, findById, create, update,
+    delete: del, login, checkOwner
+} = require('../controllers/users');
+
+const { secret } = require('../config');
+
+const auth = async (ctx, next) => {
+    const { authorization = '' } = ctx.request.header; // 短路语法，默认为空字符串
+    const token = authorization.replace('Bearer ', '');
+    try {
+        const user = jsonwebtoken.verify(token, secret);
+        ctx.state.user = user; // ctx.state通常用来存放用户信息
+    } catch (err) {
+        ctx.throw(401, err.message);
+    }
+    await next();
+}
 
 router.get('/', find);
 router.post('/', create);
 router.get('/:id', findById);
-router.patch('/:id', update); // put 是整体替换， patch 是部分替换
-router.delete('/:id', del);
+router.patch('/:id', auth, checkOwner, update); // put 是整体替换， patch 是部分替换
+router.delete('/:id', auth, checkOwner, del);
 router.post('/login', login);
 
 module.exports = router;
